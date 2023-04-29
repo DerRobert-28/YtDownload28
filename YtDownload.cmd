ac65bed08f699e58d6dd38180068aceb0c47a560
Der Robert <erictainment@live.de>
2023-04-30 01:57:03 +0200

    Update YtDownload.cmd

--- YtDownload.cmd
+++ YtDownload.cmd
@@ -1,5 +1,11 @@
 @echo off
-mode con cols=80 lines=25
-echo Enter the URL of the YouTube video:
-python YtDownload.py
-pause
+ mode con cols=80 lines=25
+ color f0
+ title Youtube Downloader
+ echo.
+ echo Enter the URL of the YouTube video:
+ echo.
+ python YtDownload.py
+ echo.
+ pause
+:end
