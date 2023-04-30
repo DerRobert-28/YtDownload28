0650f567b7a4a521e1290f22e0ddc006810bdcd6
Der Robert <erictainment@live.de>
2023-04-30 02:06:47 +0200

    loop download

--- YtDownload.cmd
+++ YtDownload.cmd
@@ -2,10 +2,17 @@
  mode con cols=80 lines=25
  color f0
  title Youtube Downloader
+:loop
+ cls
  echo.
  echo Enter the URL of the YouTube video:
  echo.
  python YtDownload.py
  echo.
- pause
+ echo.
+ echo Ready.
+ echo Hit any key to download another video.
+ echo Close window to quit.
+ pause >nul
+ goto loop
 :end
