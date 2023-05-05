6ab634672f6112ee27d3c2ecec86fe993fb48b72
Der Robert <erictainment@live.de>
2023-05-05 22:06:25 +0200

    compress everything to a cmd script

--- YtDownload.cmd
+++ YtDownload.cmd
@@ -2,7 +2,9 @@
  mode con cols=80 lines=25
  color f0
  title Youtube Downloader
-:loop
+ if not exist .\Downloads goto DOWNLOADS
+ if not exist .\YtDownload.py goto PYTHON
+:LOOP
  cls
  echo.
  echo Enter the URL of the YouTube video:
@@ -14,5 +16,16 @@
  echo Hit any key to download another video.
  echo Close window to quit.
  pause >nul
- goto loop
-:end
+ goto LOOP
+:DOWNLOADS
+ md .\Downloads
+ echo. >.\Downloads\.DOWNLOAD.FOLDER
+ goto LOOP
+:PYTHON
+ echo import youtube_dl >.\YtDownload.py
+ echo url = input() >>.\YtDownload.py
+ echo ydl_opts = {'outtmpl': './Downloads/%%(title)s.%%(ext)s' } >>.\YtDownload.py
+ echo with youtube_dl.YoutubeDL(ydl_opts) as ydl: >>.\YtDownload.py
+ echo     ydl.download([url])  >>.\YtDownload.py
+ goto LOOP
+:END
--- YtDownload.py
+++
@@ -1,5 +0,0 @@
-import youtube_dl
-url = input()
-ydl_opts = {'outtmpl': './Downloads/%(title)s.%(ext)s' }
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
